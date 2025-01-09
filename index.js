db8d24a778e52f7296dc67fe3721e05bcb290bb2
Brandon Haupt <BrandonLHaupt@gmail.com>
2025-01-09 15:11:55 -0600

    Simon Game 1.2
    - setting up random numbers and colors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 let level = 0;
 
-let colorArray = [];
-$(".btn").each(() => {
-  colorArray.push($(".btn").attr("id"));
-  console.log(colorArray);
-});
+let buttonColors = ["red", "green", "blue", "yellow"];
+let randomChosenColor = nextSequence();
+
+
+// $(".btn").each((e) => {
+//   // colorArray.push($(e).attr("id"));
+//   // console.log(e)
+// });
 
 $("h1").text("Level: " + level);
 
@@ -12,6 +15,18 @@ $("body").keypress((e) => {
   console.log(e.key);
 });
 
-$(".btn").click(() => {
-  console.log("clicked");
-});
+$(".btn").click(colorClicked);
+
+function colorClicked(e) {
+  let className = e.currentTarget.classList[1];
+  console.log(className + " was clicked");
+}
+
+function nextSequence() {
+  let randomNumber = [];
+  let randomNum = Math.floor(Math.random() * 4);
+  randomNumber.push(randomNum);
+  console.log(randomNumber);
+}
+
+nextSequence();
